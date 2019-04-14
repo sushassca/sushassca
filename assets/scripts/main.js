cbb5c8ed237fef592068a90f6f591634b13ac8c5
sushassca <sushassca@gmail.com>
2019-04-14 12:37:59 +0100

    asd

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {
-
+var animation = true;
 $(".toggle").click(function() {
-  console.log("asd");
+
+  if (animation === true) {
+    animation = false;
+    $("#menu").toggleClass("hide");
+    $("#wrapper").toggleClass("full");
+    setTimeout(function () {
+        $(".toggle").toggleClass("toggle_up");
+        animation = true;
+    }, 600);
+  } else {
+
+  }
+
 });
 
 
